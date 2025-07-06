0f90fc009f43381dbfb5d0fac32a9498c9bfe222
GuilhermeFontanella <gui.jmf@hotmail.com>
2025-07-06 19:28:41 -0300

    host app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  {
+    path: 'mfe',
+    loadChildren: () => 
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './TestModule',
+      }).then(m => m.TestModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
