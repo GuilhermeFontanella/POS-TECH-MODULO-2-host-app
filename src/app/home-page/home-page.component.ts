5cd3d294cfcf0a93e774dc801e6b2b48128bac79
GuilhermeFontanella <65752491+GuilhermeFontanella@users.noreply.github.com>
2025-08-04 23:39:34 -0300

    Update home-page.component.ts

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Component, HostListener, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { checkScreenSize, ScreenType } from 'src/utils/check-screen-size';
+import { checkScreenSize, ScreenType } from 'src/utils/check-screen-size'
 
 @Component({
   selector: 'app-home-page',
@@ -8,40 +8,39 @@ import { checkScreenSize, ScreenType } from 'src/utils/check-screen-size';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  @ViewChild('mfeSidemenu', { read: ViewContainerRef })
+  @ViewChild('homePageRef', { read: ViewContainerRef })
         homePageRef!: ViewContainerRef;
   @HostListener('window:resize', ['$event']) onWindowResize() {
     this.screenType = checkScreenSize(window.innerWidth);
     this.rebuildMfeComponents();
   }
-  @ViewChild('mfeHome', { read: ViewContainerRef }) 
-    homeRef!: ViewContainerRef;
 
   public screenType: ScreenType = 'desktop';
   private homeComponent: any;
-  private componentExtrato: any;
 
   constructor(private injector: Injector) {
     this.screenType = checkScreenSize(window.innerWidth);
   }
 
   ngOnInit(): void {
+    
     this.getMfeHome();
   }
 
   async getMfeHome() {
     this.homeComponent = await loadRemoteModule({
       type: 'module',
-      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      remoteEntry: 'https://mfe-home-hwcqe3hgg4avhbbe.canadacentral-01.azurewebsites.net/remoteEntry.js', /* alterar para http://localhost:4202/remoteEntry.js */
       exposedModule: './HomeComponent',
     }).then((m: any) => m.HomeComponent);
 
     console.log(this.homeComponent)
 
-    this.homeRef.createComponent(this.homeComponent, {
+    this.homePageRef.createComponent(this.homeComponent, {
       injector: this.injector,
     });
   }
+
   private rebuildMfeComponents() {
     setTimeout(() => {
       
