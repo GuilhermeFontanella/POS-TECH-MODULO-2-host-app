e52864703ee85f28143765f4e979b4b938759561
GuilhermeFontanella <65752491+GuilhermeFontanella@users.noreply.github.com>
2025-08-04 23:27:10 -0300

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,12 @@
-module.exports = require('./webpack.config');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    "mfeRemote": 'https://mfe-navbar-byhfgzhsf4dmdkdc.canadacentral-01.azurewebsites.net/remoteEntry.js',
+    "mfeHome": 'https://mfe-home-hwcqe3hgg4avhbbe.canadacentral-01.azurewebsites.net/remoteEntry.js',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  }
+});
